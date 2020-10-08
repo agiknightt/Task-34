4bc70de7d86f94b382f999b9794ea71dc0df3639
agiknightt <70384293+agiknightt@users.noreply.github.com>
2020-10-08 15:03:26 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -14,13 +14,14 @@ namespace Task_34
 
             Console.Clear();
 
-            shop.ServiceClient(product);
+            shop.ServiceClients(product);
         }
     }
     class Client
     {
         private int _money;
-        private List<Product> _product = new List<Product>();
+        private int _price;
+        private List<Product> _products = new List<Product>();
         public Client(int money)
         {
             _money = money;          
@@ -29,12 +30,17 @@ namespace Task_34
         {
             ComputeAllSumProducts();
 
-            while(_product.Count > 0)
+            if (_money > _price)
             {
-                if (_money >= _product[0].Price)
+                Console.WriteLine("Клиент покупает все продукты.");
+            }
+
+            while (_products.Count > 0)
+            {
+                if (_money >= _products[0].Price)
                 {                    
-                    _money -= _product[0].Price;
-                    _product.RemoveAt(0);
+                    _money -= _products[0].Price;
+                    _products.RemoveAt(0);
                 }
                 else
                 {
@@ -42,42 +48,43 @@ namespace Task_34
                 }
             }            
         }
-        public void AddAndShowProducts(Product[] products)
+        public void AddProducts(Product[] products)
         {
             Random rand = new Random();
 
-            Console.Write("В корзине у клиента : ");
-
             for (int i = 0; i < rand.Next(1, 5); i++)
             {
-                int numberProduct = rand.Next(0, products.Length);                
-                _product.Add(new Product(products[numberProduct].Name, products[numberProduct].Price));
-                Console.Write($" {_product[i].Name}. ");
+                int numberProduct = rand.Next(0, products.Length);
+                _products.Add(new Product(products[numberProduct].Name, products[numberProduct].Price));
             }
         }
-        public void TakeAwayProduct()
+
+        public void ShowProducts()
+        {
+            Console.Write("В корзине у клиента : ");
+
+            for (int i = 0; i < _products.Count; i++)
+            {
+                Console.Write($" {_products[i].Name}. ");
+            }            
+        }
+        private void TakeAwayProduct()
         {
             Random rand = new Random();
 
             Console.WriteLine("Клиенту не хватает денег для покупки.");
-            int numberProduct = rand.Next(0, _product.Count);
-            Console.WriteLine($"Клиент убирает из корзины {_product[numberProduct].Name}.");
-            _product.RemoveAt(numberProduct);
+            int numberProduct = rand.Next(0, _products.Count);
+            Console.WriteLine($"Клиент убирает из корзины {_products[numberProduct].Name}.");
+            _products.RemoveAt(numberProduct);
         }
-        public void ComputeAllSumProducts()
+        private void ComputeAllSumProducts()
         {
-            int prices = 0;
-            for (int i = 0; i < _product.Count; i++)
+            for (int i = 0; i < _products.Count; i++)
             {
-                prices += _product[i].Price;
-            }            
-            Console.WriteLine($"\nИтоговая сумма покупки {prices}, у клента {_money} денег.\n");
-
-            if (_money > prices)
-            {
-                Console.WriteLine("Клиент покупает все продукты.");
+                _price += _products[i].Price;
             }
-        }
+            Console.WriteLine($"\nИтоговая сумма покупки {_price}, у клента {_money} денег.\n");
+        }        
     }
     class Product 
     {
@@ -116,11 +123,12 @@ namespace Task_34
                 _clients.Enqueue(new Client(rand.Next(10, 30)));                
             }
         }
-        public void ServiceClient(Product[] products)
+        public void ServiceClients(Product[] products)
         {
             while (_clients.Count > 0)
             {
-                _clients.Peek().AddAndShowProducts(products);
+                _clients.Peek().AddProducts(products);
+                _clients.Peek().ShowProducts();
                 _clients.Dequeue().BuyProducts();
                 Console.WriteLine("Для продолжения нажмите любую клавишу...");
                 Console.ReadKey();
